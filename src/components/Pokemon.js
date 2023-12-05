2637662748e26b165b780f34961b6012a0699a3b
Pwikster <pwkjr1@gmail.com>
2023-12-04 23:28:05 -0600

    Changed to axios

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,12 +1,12 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Pokemon = (props) => {
     const [pokemon, setPokemon] = useState([])
 
     useEffect(() => {
-        fetch('https://pokeapi.co/api/v2/pokemon?limit=807')
-            .then(response => response.json())
-            .then(data => setPokemon(data.results))
+        axios.get('https://pokeapi.co/api/v2/pokemon?limit=807')
+            .then(response => setPokemon(response.data.results))
             .catch(error => console.error('Error', error))
     }, []);
 
